cd8df2fdb98102555e5fe65a925c0c4991dae34e
Richie <richard_wu94@hotmail.com>
2015-07-13 23:10:30 -0600

    updated  login and register page

--- app/routes.js
+++ app/routes.js
@@ -17,7 +17,7 @@ module.exports = function(app, passport) {
     app.get('/login', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('login.ejs', { message: req.flash('loginMessage') }); 
+        res.render('pages/login.ejs', { message: req.flash('loginMessage') }); 
     });
 
     // process the login form
@@ -30,7 +30,7 @@ module.exports = function(app, passport) {
     app.get('/signup', function(req, res) {
 
         // render the page and pass in any flash data if it exists
-        res.render('signup.ejs', { message: req.flash('signupMessage') });
+        res.render('pages/signup.ejs', { message: req.flash('signupMessage') });
     });
 
     // process the signup form
@@ -41,7 +41,7 @@ module.exports = function(app, passport) {
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
-    app.get('/profile', function(req, res) {
+    app.get('/profile', isLoggedIn, function(req, res) {
         res.render('pages/profile.ejs', {
             user : req.user // get the user out of session and pass to template
         });
